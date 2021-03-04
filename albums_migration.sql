efd08d8aeb24ab0f7bfe200ba0171184207ff894
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-03-04 14:12:46 -0600

    updating migration exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,12 @@
 use codeup_tests_db;
 
-drop table if exits albums;
+drop table is exists albums;
 
 CREATE TABLE if not exists albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) not null,
     name VARCHAR(100) NOT NULL,
-    release_date DATE not null,
+    date INT not null,
     sales decimal (6,3) not null,
     genre VARCHAR(100) not null,
     PRIMARY KEY (id)
